00b438681bc394578abcf69ccb50f8aec69bde45
Ja7Ca <76737729+Ja7Ca@users.noreply.github.com>
2021-03-18 16:23:16 +0700

    ~

---
+++ Modul 2/Latihan/LatihanModul2-5.py
@@ -0,0 +1,13 @@
+import LatihanModul2 as LM2
+
+class MhsTIF(LM2.Mahasiswa):
+    def katakanPy(self):
+        print('Python is cool.')
+
+##m4 = MhsTIF('Badu',334,'Sragen',230000)
+##m4.katakanPy()
+##print(m4)
+##print(m4.keadaan)
+##m4.makan('pecel')
+##print(m4.keadaan)
+##m4.ucapkanSalam()
